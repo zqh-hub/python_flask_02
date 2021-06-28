28f324c42b1b5013c3c19665d8a9c5c4ee44a0d9
zqh-hub <going163@163.com>
2021-06-28 21:22:26 +0800

    案例更新

--- apps/user/view.py
+++ apps/user/view.py
@@ -13,7 +13,7 @@ def index():
 users = []
 
 
-@user_bp.route("/user/reg", methods=["GET", "POST"])
+@user_bp.route("/user/reg", methods=["GET", "POST"], endpoint="reg")
 def register():
     if request.method == "POST":
         username = request.form.get("username")
@@ -28,17 +28,36 @@ def register():
     return render_template("user/register.html")
 
 
-@user_bp.route("/user/show")
+@user_bp.route("/user/show", endpoint="show")
 def show():
     return render_template("user/show.html", users=users)
 
 
-@user_bp.route("/user/del")
-def dele():
+@user_bp.route("/user/del", endpoint="del")
+def delete_user():
     name = request.args.get("username")
     for i in users:
         if name == i.username:
             users.remove(i)
-            return redirect("/user/show")
+            # return redirect("/user/show")
+            return redirect(url_for("user.show"))
     else:
         return "错误"
+
+
+@user_bp.route("/user/update", methods=["GET", "POST"], endpoint="update")
+def user_update():
+    if request.method == "POST":
+        rel_username = request.form.get("rel_username")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        for user in users:
+            if user.username == rel_username:
+                user.username = username
+                user.password = password
+                return redirect(url_for("user.show"))
+    else:
+        update_username = request.args.get("username")
+        for user in users:
+            if user.username == update_username:
+                return render_template("user/update.html", user=user)
